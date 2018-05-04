9e9a098ceb7531796036da3f4741f13869a0ced9
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-05-04 17:06:34 +0100

    removed Pod directory

--- Pods/Target Support Files/ImagePicker/ImagePicker-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double ImagePickerVersionNumber;
-FOUNDATION_EXPORT const unsigned char ImagePickerVersionString[];
-
--- Pods/Target Support Files/LBTAComponents/LBTAComponents-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LBTAComponents : NSObject
-@end
-@implementation PodsDummy_LBTAComponents
-@end
